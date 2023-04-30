b694b9af2da6a164729394ee0d6c3cfa4c06dd11
Letícia Baião <102307053+LeBaiao@users.noreply.github.com>
2023-04-30 14:13:38 -0300

    Add files via upload

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,15 @@ const connection = require('../database/connection')
 const express = require('express')
 const router = express.Router()
 const clienteController = require('../controllers/clienteController')
+const pedidoController = require('../controllers/pedidoController')
 
 router.post('/novoCliente', clienteController.novoCliente)
 router.get('/clientes', clienteController.listarClientes)
 router.get('/cliente/:id', clienteController.listarUmCliente)
 router.put('/atualizar/cliente/:id', clienteController.atualizarCliente)
 router.delete('/delete/cliente/:id', clienteController.removerCliente)
+
+
+router.post('/novoPedido', pedidoController.novoPedido);
 
 module.exports = router

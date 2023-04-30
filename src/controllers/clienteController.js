25984987a29977fd4ba4ae385c70d93b055f580a
Letícia Baião <102307053+LeBaiao@users.noreply.github.com>
2023-04-30 13:42:54 -0300

    Add files via upload

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -14,6 +14,45 @@ response.json({message:"Cliente criado com sucesso!"})
             console.log(error)
         })
     }
+
+    listarClientes(request, response){
+        database.select("*").table("cliente_tb").then(clientes=>{
+            console.log(clientes)
+            response.json(clientes)
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
+    listarUmCliente(request, response){
+        const id = request.params.id;
+        database.select("*").table("cliente_tb").where({id:id}).then(cliente=>{
+            response.json(cliente)
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
+
+    atualizarCliente(request, response){
+        const id = request.params.id
+        const {cpf, nome} = request.body
+        database.where({id:id}).update({cpf:cpf, nome:nome})
+        .table("cliente_tb")
+        .then(data=>{
+            response.json({message:"Cliente atualizado com sucesso"})
+        }).catch(error=>{
+            response.json(error)
+        })
+    }
+
+    removerCliente(request,response){
+        const id = request.params.id
+
+        database.where({id:id}).del().table("cliente_tb").then(data=>{
+            response.json({message:"Cliente removido com sucesso"})
+        }).catch(error=>{
+            response.json(error)
+        })
+    }
 }
 
 module.exports = new clienteController()

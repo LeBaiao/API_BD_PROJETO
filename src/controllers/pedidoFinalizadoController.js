116332d956c23a274b3f866c7b93c2e1d65744c3
Letícia Baião <102307053+LeBaiao@users.noreply.github.com>
2023-05-28 14:35:39 -0300

    update de pedido finalizado :p

---
+++ src/controllers/pedidoFinalizadoController.js
@@ -0,0 +1,116 @@
+//Para a tabela de pedidos finalizados só será possível utilizar os métodos POST e GET
+const database = require('../database/connection')
+
+
+class pedidoFinalizadoController {
+
+      listarPedidosFinalizados(request, response) {
+        database
+          .select(
+            'pedido_finalizado_tb.id_pedido',  
+            'pedido_finalizado_tb.data_pedido',
+            'pedido_finalizado_tb.valor_total',
+            'cliente_tb.id',
+            'cliente_tb.nome',
+            'pedido_finalizado_tb.forma_pagamento'
+          )
+          .from('cliente_tb')
+          .join('pedido_tb', 'cliente_tb.id', '=', 'pedido_tb.id_cliente')  //faz o join com pedido_tb e cliente_tb para pegar os dados do cliente
+          .join('pedido_finalizado_tb', 'pedido_tb.id', '=', 'pedido_finalizado_tb.id_pedido')
+          .orderBy('pedido_finalizado_tb.id_pedido') // Ordena os resultados pelo ID do pedido
+          .then(pedidos => {
+            const pedidosSeparados = {};
+            pedidos.forEach(pedido => {  //percorrendo o array de pedidos e organizando-os em um objeto pedidosSeparados com base no campo id_pedido.
+              const idPedido = pedido.id_pedido;
+              if (!pedidosSeparados[idPedido]) {
+                pedidosSeparados[idPedido] = {
+                  id_pedido: pedido.id_pedido,
+                  data_pedido: pedido.data_pedido,
+                  valor_total: pedido.valor_total,
+                  forma_pagamento: pedido.forma_pagamento,
+                  cliente: {  //formatação do json
+                    id: pedido.id,
+                    nome: pedido.nome
+                  }
+                 
+                };
+              }
+            });
+      
+            const resultado = Object.values(pedidosSeparados);
+            response.json(resultado);
+          })
+          .catch(error => {
+            console.log(error);
+            response.status(500).json({ error: 'Ocorreu um erro ao listar os pedidos finalizados' });
+          });
+      }
+      
+
+//funcionando
+listarUmPedidoFinalizado(request, response) {
+    const id = request.params.id;
+    database
+      .select(
+        'pedido_finalizado_tb.id_pedido',
+        'pedido_finalizado_tb.data_pedido',
+        'pedido_finalizado_tb.valor_total',
+        'cliente_tb.id',
+        'cliente_tb.nome',
+        'pedido_finalizado_tb.forma_pagamento'
+      )
+      .from('cliente_tb')
+      .join('pedido_tb', 'cliente_tb.id', '=', 'pedido_tb.id_cliente')
+      .join('pedido_finalizado_tb', 'pedido_tb.id', '=', 'pedido_finalizado_tb.id_pedido')
+      .where('pedido_finalizado_tb.id_pedido', id)
+      .then(cliente => {
+        response.json(cliente);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  //funcionando
+  listarPedidoByCliente(request, response) {
+    const id = request.params.id;
+    database
+      .select(
+        'pedido_finalizado_tb.id_pedido',
+        'pedido_finalizado_tb.data_pedido',
+        'pedido_finalizado_tb.valor_total',
+        'cliente_tb.id',
+        'cliente_tb.nome',
+        'pedido_finalizado_tb.forma_pagamento'
+      )
+      .from('cliente_tb')
+      .join('pedido_tb', 'cliente_tb.id', '=', 'pedido_tb.id_cliente')
+      .join('pedido_finalizado_tb', 'pedido_tb.id', '=', 'pedido_finalizado_tb.id_pedido')
+      .where('pedido_tb.id_cliente', id )
+      .then(cliente => {
+        response.json(cliente);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  //funcionando
+   novoPedidoFinalizado = (req, res) => {
+    const { id_pedido, data_pedido, valor_total, forma_pagamento } = req.body;
+  
+    database('pedido_finalizado_tb')
+      .insert({ id_pedido, data_pedido, valor_total, forma_pagamento })
+      .then(() => {
+        res.json({ message: 'Pedido finalizado com sucesso!' });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: 'Ocorreu um erro ao finalizar o pedido.' });
+      });
+  };
+
+
+
+}
+module.exports = new pedidoFinalizadoController()
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,23 +4,36 @@ const router = express.Router()
 const clienteController = require('../controllers/clienteController')
 const produtoController = require('../controllers/produtoController')
 const pedidoController = require('../controllers/pedidoController')
+const pedidoFinalizadoController = require('../controllers/pedidoFinalizadoController')
 
+//rotas de cliente
 router.post('/novoCliente', clienteController.novoCliente)
 router.get('/clientes', clienteController.listarClientes)
 router.get('/cliente/:id', clienteController.listarUmCliente)
 router.put('/atualizar/cliente/:id', clienteController.atualizarCliente)
 router.delete('/delete/cliente/:id', clienteController.removerCliente)
 
+
+//rotas de produto
 router.post('/novoProduto', produtoController.novoProduto)
 router.get('/produtos', produtoController.listarProduto)
 router.get('/produto/:id', produtoController.listarUmProduto)
 router.put('/atualizar/produto/:id', produtoController.atualizarProduto)
 router.delete('/deletar/produto/:id', produtoController.removerProduto)
 
+
+//rotas de pedido
 router.post('/novopedido', pedidoController.novoPedido);
 router.get('/pedidos', pedidoController.getPedidos);
 router.put('/atualizar/pedido/:id', pedidoController.atualizaPedido);
 router.get('/pedido/:id', pedidoController.getPedido);
 router.delete('/delete/pedido/:id', pedidoController.removerPedido);
+
+//rotas de pedidos finalizados
+router.get('/pedidosfinalizados', pedidoFinalizadoController.listarPedidosFinalizados); 
+router.get('/pedidofinalizado/:id', pedidoFinalizadoController.listarUmPedidoFinalizado); //busca pedido por id do pedido
+router.get('/pedidofinalizado/cliente/:id', pedidoFinalizadoController.listarPedidoByCliente); //buscar pedido finalizado por cliente
+router.post('/novopedidofinalizado', pedidoFinalizadoController.novoPedidoFinalizado);
+
 
 module.exports = router
